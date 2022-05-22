139d4fe77a1cedf7256a68a0abf961c95e6be965
Haroun B <haroun@harryb.dev>
2022-05-22 17:39:33 +0200

    fixes style without touch buttons

--- js/main.js
+++ js/main.js
@@ -131,17 +131,17 @@ const game = {
 game.buildCells();
 console.log(game.width, game.height);
 
-visualViewport.addEventListener(`resize`, e => {
-  [game.playgroundElement, game.phContainer, game.spContainer].forEach(container => {
-    if (visualViewport.width < visualViewport.height) {
-      container.style.width = `100vw`;
-      container.style.height = `${(game.height / game.width) * 100}vw`;
+// visualViewport.addEventListener(`resize`, e => {
+//   [game.playgroundElement, game.phContainer, game.spContainer].forEach(container => {
+//     if (visualViewport.width < visualViewport.height) {
+//       container.style.width = `100vw`;
+//       container.style.height = `${(game.height / game.width) * 100}vw`;
 
       
-    }
-    if (visualViewport.width > visualViewport.height) {
-      container.style.height = `100vh`;
-      container.style.width = `${(game.width / game.height) * 100}vh`;
-    }
-  });
-});
+//     }
+//     if (visualViewport.width > visualViewport.height) {
+//       container.style.height = `100vh`;
+//       container.style.width = `${(game.width / game.height) * 100}vh`;
+//     }
+//   });
+// });
